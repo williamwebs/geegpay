a8b568f58dd7e7af908de3377262e983d744115c
William <anazawilliam1@gmail.com>
2024-01-27 16:03:41 +0100

    remove file

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,23 +1,23 @@
-import clientPromise from "@/libs/mongoClient";
-import nextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+// import clientPromise from "@/libs/mongoClient";
+// import nextAuth from "next-auth";
+// import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions = {
-  adapter: MongoDBAdapter(clientPromise),
-  session: {
-    strategy: "jwt",
-  },
-  pages: {
-    signIn: "/",
-  },
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-  ],
-};
+// export const authOptions = {
+//   adapter: MongoDBAdapter(clientPromise),
+//   session: {
+//     strategy: "jwt",
+//   },
+//   pages: {
+//     signIn: "/",
+//   },
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     }),
+//   ],
+// };
 
-const handler = nextAuth(authOptions);
+// const handler = nextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+// export { handler as GET, handler as POST };
